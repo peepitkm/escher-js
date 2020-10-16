ed95f1a0257e589b2ae8f09ca9f9bb75ce8140fd
peepitkm <pornpat.pp@gmail.com>
2020-10-16 10:34:00 +0700

    Add webpack minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 // Webpack uses this to work with directories
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const name = isDev ? 'escher' : 'escher.min';
 
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
@@ -8,9 +12,11 @@ module.exports = {
 
   // Path to your entry point. From this file Webpack will begin his work
   entry: {
-    'escher': ['./lib/escher.js'],
-    'escher.min': ['./lib/escher.js']
+    [name]: ['./lib/escher.js']
   },
+
+  devtool: isDev ? 'inline-source-map' : false,
+  stats: { warnings: false }, // Hide warnings
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
@@ -25,6 +31,31 @@ module.exports = {
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript 
   // minifying and other thing so let's set mode to development
-  mode: 'production'
+  mode: process.env.NODE_ENV || 'development',
+
+  optimization: {
+    chunkIds: isDev ? 'named' : 'total-size',
+    minimize: !isDev,
+    minimizer: isDev
+      ? []
+      : [
+          new UglifyJsPlugin({
+            // include: [path.resolve(__dirname, '../src')],
+            cache: true,
+            parallel: true,
+            extractComments: true,
+            uglifyOptions: {
+              warnings: false,
+              parse: {},
+              compress: {},
+              mangle: true,
+              output: null,
+              ie8: false,
+              keep_fnames: false,
+              toplevel: false,
+            },
+          }),
+        ],
+  },
 
 };
